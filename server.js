311174de2fd12b0ff4ee671c038e92de42f429d4
D4fto <163424876+D4fto@users.noreply.github.com>
2024-09-29 19:49:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -36,9 +36,15 @@ io.on('connection', (socket) => {
         console.log('Mensagem recebida:', msg); // Adicione este log
         if (!users[socket.id]) {
             users[socket.id] = msg;
-            io.emit('message', `${msg} entrou no chat.`);
+            io.emit('message', JSON.stringify({
+                msg: `${msg} entrou no chat.`,
+                user: 'servidor'
+            }));
         } else {
-            io.emit('message', `${users[socket.id]}: ${msg}`);
+            io.emit('message', JSON.stringify({
+                msg: msg,
+                user: users[socket.id]
+            }));
         }
     });
 
