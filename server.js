b71653dd913991496b54a853ebf2ba918cbeda62
D4fto <163424876+D4fto@users.noreply.github.com>
2024-09-29 21:10:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const {connection} = require('/mysql/connection')
+const {connection} = require('./mysql/connection')
 
 const app = express();
 const server = http.createServer(app);
