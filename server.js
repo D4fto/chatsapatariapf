44368c920c3c008538333a36a02431179849fe4b
D4fto <163424876+D4fto@users.noreply.github.com>
2024-10-02 10:03:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const {connection} = require('./mysql/connection')
 const app = express();
 const server = http.createServer(app);
 
-
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conectado ao banco de dados!');
+  });
 
 app.use(cors({
     origin: 'https://sapatariapf.onrender.com',
